ac1a08032c8a8078bc96b683709d17d7648e1a22
Glynn-Taylor <glynn.taylor@warwick.ac.uk>
2014-11-20 03:45:59 +0000

    Added OpenGUI

---
+++ Assets/Plugins/OpenGUI/Scripts/OGPage.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+public class OGPage extends MonoBehaviour {
+	public var pageName : String = "New Page";
+
+	public function StartPage () {}
+	public function UpdatePage () {}
+	public function ExitPage () {}
+	public function DrawPage () {}
+
+	public function UpdateStyles () {
+		for ( var w : OGWidget in this.transform.GetComponentsInChildren.<OGWidget>(true) ) {
+			if ( !w.root ) {
+				w.root = OGRoot.GetInstance();
+			}
+
+			if ( !w.styles ) {
+				w.ApplyDefaultStyles ();
+			}
+			
+			w.styles.Refresh ( w.root.skin );
+		}
+	}
+
+	public function ResetStyles () {
+		for ( var w : OGWidget in this.transform.GetComponentsInChildren.<OGWidget>(true) ) {
+			w.ApplyDefaultStyles ();
+		}	
+	}
+
+}
--- README.md
+++ README.md
@@ -12,5 +12,3 @@ Still in development
     - [Games](https://www.warwickgamedesign.co.uk/games)
 
 ##Builds
-    - [Windows](https://dl.dropboxusercontent.com/u/52640380/Released_Builds/code-cracker/codecracker_windows_08-11-2014.zip)
-    - [Linux](https://dl.dropboxusercontent.com/u/52640380/Released_Builds/code-cracker/codecracker_linux_08-11-2014.zip)
